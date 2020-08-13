84354c0047df6bd14b3de9a4d70d47be8a5e7172
Beau Taub <beau.taub@bayshorenetworks.com>
2020-08-12 23:25:17 -0400

    add conditional check, remove make post from timeline

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-document.getElementById("pagelet_composer").remove();
+if (typeof pagelet_composer != "undefined") {
+    document.getElementById("pagelet_composer").remove();
+}
+
+if (typeof pagelet_timeline_composer != "undefined") {
+    document.getElementById("pagelet_timeline_composer").remove();
+}
